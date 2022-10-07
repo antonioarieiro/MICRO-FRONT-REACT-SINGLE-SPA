7f981f8e94c9ad683c8bd0088fa03da801a81c93
||ȼhaiɳ <sfgfffs@gmail.com>
2022-10-07 00:06:19 -0300

    :recycle: add first mfe dependencies and set tailwind config :recycle:

---
+++ kinergy-front/webpack.config.js
@@ -0,0 +1,48 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "kinergy",
+    projectName: "kinergy",
+    webpackConfigEnv,
+    argv,
+    
+  });
+
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+   
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude:
+          /node_modules/,
+        use: {
+          loader:
+            'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-react',
+              '@babel/preset-env',
+            ],
+            plugins: [
+              '@babel/plugin-transform-runtime',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+ 
+  });
+};
